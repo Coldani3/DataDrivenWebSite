895eeb754a073dc73d822888414c0810fec7dbc2
Coldani3 <p110117069@gmail.com>
2020-11-08 17:38:59 +0000

    More work on carpage, starting to get a feel for how it'll end up working

--- carpage.js
+++ carpage.js
@@ -17,7 +17,16 @@ const CarPageApp = {
             //TODO: get from page parameters
             //TODO: get car image automatically without passing it through GET
             this.carIndex = url.searchParams("carIndex");//sessionStorage.getItem("carImage");
-            //
+
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200)
+                {
+                    document.getElementById("mainBox").innerHTML = this.responseText;
+                }
+            }
+            xhttp.open("GET", "carpage.php?carIndex=" + this.carIndex, true);
+            xhttp.send();
         }
     }
 }
--- carpage.php
+++ carpage.php
@@ -1,3 +1,31 @@
 <?php
+$host = 'localhost';
+$db   = 'carsdatabase';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [    
+PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,    
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,    
+PDO::ATTR_EMULATE_PREPARES   => false,];
+
+try 
+{    
+	$pdo = new PDO($dsn, $user, $pass, $options);
+} 
+catch (\PDOException $e) 
+{     
+	throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$query = $pdo->query("SELECT * FROM cars WHERE carIndex = ".$_GET["carIndex"]);
+
+$row = $query.fetch();
+
+echo "<img href='".$row["image"]."' alt='Car'>";
+
+echo "<div><button class='button' id='purchaseButton'>Purchase</button></div>"
 
 ?>
