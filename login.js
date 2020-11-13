811f9b410ca6660abdb054bd853d6446871d21c0
Coldani3 <p110117069@gmail.com>
2020-11-13 20:54:37 +0000

    Added logging in. Testing required.

--- login.js
+++ login.js
@@ -2,11 +2,21 @@ function login()
 {
     var usernameInput = document.getElementById("username");
     var passwordInput = document.getElementById("password");
+
+    sessionStorage.setItem("username", usernameInput);
+    sessionStorage.setItem("password", passwordInput);
+
     if (usernameInput.value.length > 0 && passwordInput.value.length > 0)
     {
         var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                
+            }
+        }
         
-        xhttp.open("GET", "php/login.php?username=" + usernameInput.value + "&password=" /* BAD, NEVER DO THIS */ + passwordInput.value, true);
+        xhttp.open("GET", "php/login.php", true);
         xhttp.send();
     }
 }
--- php/login.php
+++ php/login.php
@@ -1,10 +1,51 @@
 <?php
 session_start();
 
-if (isset($_GET["username"]) && isset($_GET["password"]))
+$host = 'localhost';
+$db   = 'carsdatabase';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [    
+PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,    
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,    
+PDO::ATTR_EMULATE_PREPARES   => false,];
+
+try 
+{    
+	$pdo = new PDO($dsn, $user, $pass, $options);
+} 
+catch (\PDOException $e) 
+{     
+	throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$username = $_SESSION["username"];
+$password = $_SESSION["password"];
+
+$query = $pdo->query("SELECT * FROM users WHERE username = $username AND password = $password");
+
+if ($query)
 {
-    $_SESSION["username"] = $_GET["username"];
-    //very bad don't do this, replace with actual login
-    $_SESSION["password"] = $_GET["password"];
+    echo "1";
+}
+else
+{
+    echo "2";
+}
+
+$result = $query->fetch();
+
+$_SESSION["userID"] = $result["userID"];
+
+if ($result["admin"] == 1)
+{
+    $_SESSION["admin"] = true;
+}
+else
+{
+    $_SESSION["admin"] = false;
 }
 ?>
